1be892cd0b26c2b2e44cc82db645ec627a94e02a
LucianoFrias <69430221+LucianoFrias@users.noreply.github.com>
2022-09-09 18:55:04 -0300

    Add files via upload

--- authentication-app/authView.js
+++ authentication-app/authView.js
@@ -5,14 +5,45 @@ class AuthView extends HTMLElement
         super();
         this.innerModel = model;
 
-        this.test = document.createElement('h1');
-        this.test.innerText = 'Hello World!'
+        this.formElement = document.createElement('form');
+        this.formElement.id = 'loginContainer';
+
+        this.usernameH1 = document.createElement('h1');
+        this.usernameH1.id = 'Username';
+        this.usernameH1.innerText = 'Username'
+
+        this.usernameInput = document.createElement('input');
+        this.usernameInput.className = 'InputBox1';
+        this.usernameInput.type = 'text';
+        this.usernameInput.name = 'username';
+        this.usernameInput.id = 'username';
+
+        this.passwordH1 = document.createElement('h1');
+        this.passwordH1.id = 'Password';
+        this.passwordH1.innerText = 'Password'
+
+        this.passwordInput = document.createElement('input');
+        this.passwordInput.className = 'InputBox2';
+        this.passwordInput.type = 'password';
+        this.passwordInput.name = 'password';
+        this.passwordInput.id = 'password';
+
+        this.submitBtn = document.createElement('button');
+        this.submitBtn.id = 'LoginBtn';
+        this.submitBtn.type = 'submit';
+        this.submitBtn.innerText = 'Login';
 
     }
 
     connectedCallback()
     {
-        document.body.appendChild(this.test);
+        this.appendChild(this.formElement);
+
+        this.formElement.appendChild(this.usernameH1);
+        this.formElement.appendChild(this.usernameInput);
+        this.formElement.appendChild(this.passwordH1);
+        this.formElement.appendChild(this.passwordInput);
+        this.formElement.appendChild(this.submitBtn);
     }
 
 }
