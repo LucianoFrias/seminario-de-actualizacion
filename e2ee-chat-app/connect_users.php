b9a1a57cc29a5c82bc0d434c0c1ed567c7240e64
LucianoFrias <69430221+LucianoFrias@users.noreply.github.com>
2022-11-09 19:05:54 -0300

    See how session_start() works in PHP.

--- e2ee-chat-app/connect_users.php
+++ e2ee-chat-app/connect_users.php
@@ -7,6 +7,6 @@ $object = json_decode($json_body);
 
 $key = connect_users('A','B');
 
-echo json_encode($key)
+echo json_encode($key);
 
 ?>
--- e2ee-chat-app/server.php
+++ e2ee-chat-app/server.php
@@ -9,31 +9,31 @@ function connect_users($id_user_a, $id_user_b)
 {
   //Acá haría varias cosas.. pero al final,
   //Debe devolver la clave simétrica.
-
+  
   return generate_key($id_user_a, $id_user_b);
-
+  
 }
 
 function disconnect_users( $id_user_a, $id_user_b )
 {
-
-
+  
+  
 }
 
 function send_message($id_user_sender, $id_user_target, $body_message)
 {
-    return $body_message;
+  return $body_message;
 }
 
 function generate_key( $id_user_sender, $id_user_target)
 {
-    return hash('sha256',uniqid());
+  return hash('sha256',uniqid());
 }
 
 function get_messages( $id_user )
 {
-  session_start();
-  return echo "Mensajes: " . $_SESSION["usuario"];
+  // session_start();
+  // return echo "Mensajes: " . $_SESSION["message"];
 }
 
 ?>
